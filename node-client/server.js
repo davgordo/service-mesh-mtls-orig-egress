2398c3b8edaa426719c0c369a000e2243fbac739
Mohammad Amin <mamin@redhat.com>
2022-10-24 12:16:45 -0400

    Adding nodejs client

---
+++ node-client/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app)
+
+const PRODUCTION = app.get('env') === 'production';
+
+app.get('/', (req, res) => {
+  req.log.info({message: 'Hello from Node.js client Application!'});
+  res.send('Hello from Node.js client Application!');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send("Not Found");
+});
+
+// Listen and serve.
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`App started on PORT ${PORT}`);
+});
