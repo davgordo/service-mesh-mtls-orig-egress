1f7faeb12cc2b93a17c618f2cc7d33c3db873e89
Mohammad Amin <69978750+RHatDev@users.noreply.github.com>
2022-10-26 23:46:54 -0400

    adding todos

--- client/index.js
+++ client/index.js
@@ -3,9 +3,11 @@ const httpProxy = require('http-proxy');
 httpProxy.createProxyServer({
   target: {
     protocol: 'https:',
+    // TODO - change host to your host route
     host: 'server-demo1.apps-crc.testing',
     port: 443,
     pfx: fs.readFileSync('/opt/app-root/certs/client.p12'),
+    // TODO - move this to secret and load as env
     passphrase: 'pass123',
   },
   changeOrigin: true,
