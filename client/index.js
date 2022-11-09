1a485a4ca2578110768bf1a6384c70517a6b222a
David Gordon <davgordo@redhat.com>
2022-11-09 15:18:23 -0500

    switch to cert-manager convention for TLS secret keynames

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@ httpProxy.createProxyServer({
     // TODO - change host to your host route
     host: 'server-demo1.apps-crc.testing',
     port: 443,
-    pfx: fs.readFileSync('/opt/app-root/certs/client.p12'),
+    pfx: fs.readFileSync('/opt/app-root/certs/keystore.p12'),
     // TODO - move this to secret and load as env
     passphrase: 'pass123',
   },
